fecef0cd9137570d797b38268878694ba36900b4
PuneetAsnani12 <asnani.puneet@gmail.com>
2020-03-19 03:09:00 +0530

    created collection-preview component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import "./menu-item.styles.scss";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size ,history,match,linkUrl}) => (
-  <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
-      className='background-image'
+      className="background-image"
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
     />
-    <div className="content"> 
+    <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import './homepage.styles.scss';
-import Directory from '../../components/directory/directory.component';
-const HomePage = (props) => (
-  
+import "./homepage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+const HomePage = () => (
   <div className="homepage">
     <Directory />
-    {console.log(props)}
   </div>
 );
 
