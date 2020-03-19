4fbc60e8a2b08be35d308f3aaee3b8a6d0e30f5a
PuneetAsnani12 <asnani.puneet@gmail.com>
2020-03-19 23:52:13 +0530

    sign-in and sign-out created and styled

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -47,11 +47,12 @@ class SignIn extends React.Component {
             required
             label="Password"
           />
-
-          <CustomButton type="submit"> Sign In </CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign In with Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign In </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign In with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
