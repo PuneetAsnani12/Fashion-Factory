6d3d1b4492065a8e353b234f605cb9caba7a92c2
PuneetAsnani12 <asnani.puneet@gmail.com>
2020-03-23 22:53:27 +0530

    updated itemCount using reduce on cart reducer

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,14 +8,21 @@ import { connect } from "react-redux";
 
 import { toggleCardHidden } from "../../redux/cart/cart.actions";
 
-const CartIcon = ({ toggleCardHidden }) => (
-  <div className="cart-icon" onClick={toggleCardHidden}> 
+const CartIcon = ({ toggleCardHidden, itemCount }) => (
+  <div className="cart-icon" onClick={toggleCardHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
 const mapDispatchToProps = dispatch => ({
   toggleCardHidden: () => dispatch(toggleCardHidden())
 });
-export default connect(null, mapDispatchToProps)(CartIcon);
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  )
+});
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
