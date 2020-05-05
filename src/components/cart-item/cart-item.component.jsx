fbedda596e1750fdda4d67e95ebfa84fa30b6842
PuneetAsnani12 <asnani.puneet@gmail.com>
2020-05-05 21:17:56 +0530

    adding performance optimization to cart-item component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 import "./cart-item.styles.scss";
 
-const cartItem = ({ item: { imageUrl, price, name,quantity } }) => (
+const cartItem = ({ item: { imageUrl, price, name, quantity } }) => (
   <div className="cart-item">
     <img src={imageUrl} alt="item"></img>
     <div className="item-details">
       <span className="name">{name}</span>
-      <span className="price"> {quantity} x ${price}</span>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
     </div>
   </div>
 );
 
-export default cartItem;
+export default React.memo(cartItem);
